072ab892d012ff384c804c599e97c5586d7a7fc3
Mateus Amaral <mateus_sousa_amaral@hotmail.com>
2024-01-27 21:37:52 -0300

    Minor fixes
    Minor fixes

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
 //Ponto de Entrada da aplicação
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-
-  const config = new DocumentBuilder()
-    .setTitle('Tech Challenge API')
-    .setDescription('Tech Challenge API description')
-    .setVersion('1.0')
-    .addTag('FastFood')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 }
 bootstrap();
